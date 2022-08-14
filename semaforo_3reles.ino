77624473a46cede3e2b06c0684b67721cdd5391c
Skyroh <57754814+Skyroh@users.noreply.github.com>
2022-08-14 20:54:38 +0200

    Update semaforo_3reles.ino

--- semaforo_3reles.ino
+++ semaforo_3reles.ino
@@ -20,6 +20,8 @@ int relay3 = 4;
 
 
 // Configuración
+
+
 void setup() {
   pinMode(relay1, OUTPUT); // Configuramos que el primer relé es una salida o OUTPUT de la placa, pin "D2".
   pinMode(relay2, OUTPUT); // Configuramos que el segundo relé es una salida o OUTPUT de la placa, pin "D3".
@@ -30,42 +32,41 @@ void setup() {
  // Código principal en búcle infinito
 void loop() {
   // Encendido del relé número 1 (Luz verde)
-  digitalWrite(relay1, HIGH); // Envia señal alta (5V) al primer relé.
+  digitalWrite(relay1, LOW); // Envia señal BAJA (0V) al primer relé.
   Serial.println("Relé numero 1 encendido (Luz Verde)");
   delay(45000);           // 45 segundo encendido 
-   // Apagado del relé número 1
-  digitalWrite(relay1, LOW);  // Envia señal baja (0V) al primer relé.
+  // Apagado del relé número 1
+  digitalWrite(relay1, HIGH);  // Envia señal ALTA (5V) al primer relé.
   Serial.println("Relé 1 apagado (Luz Verde)");
   delay(3000);           // 3 segundos apagado
 
   // Encendido del relé número 3 (Luz Amarilla)
-  digitalWrite(relay3, HIGH); // Envia señal alta (5V) al tercer relé.
+  digitalWrite(relay3, LOW); // Envia señal BAJA (0V) al tercer relé.
   Serial.println("Relé numero 3 encendido(Luz Amarilla)");
   delay(45000);           // 45 segundos encendido.
   // Apagado del relé número 3
-  digitalWrite(relay3, LOW);  // Envia señal baja (0V) al relé número tres.
+  digitalWrite(relay3, HIGH);  // Envia señal ALTA (5V) al relé número tres.
   Serial.println("Relé 3 apagado (Luz Amarilla)");
   delay(3000);           // 3 segundos apagado.
 
 // Encendido de 2 relés (Luz verde y amarilla)
-  digitalWrite(relay1, HIGH); // Envia señal alta (5V) al primer relé.
-  digitalWrite(relay3, HIGH); // Envia señal alta (5V) al tercer relé.
+  digitalWrite(relay1, LOW); // Envia señal BAJA (0V) al primer relé.
+  digitalWrite(relay3, LOW); // Envia señal BAJA (0V) al tercer relé.
   Serial.println("Relé numero 1 y número 3 encendidos (Luz Amarilla y verde)");
   delay(45000);           // 45 segundos encendido
 
 // Apagado de los dos reles
-  digitalWrite(relay1, LOW);  // Envia señal baja (0V) al primer relé.
-  digitalWrite(relay3, LOW);  // Envia señal baja (0V) al relé número tres.
+  digitalWrite(relay1, HIGH);  // Envia señal ALTA (5V) al primer relé.
+  digitalWrite(relay3, HIGH);  // Envia señal ALTA (5V) al relé número tres.
   Serial.println("Relé 1 y 3 apagados (Luz Amarilla y verde)");
   delay(3000);           // 3 segundo
 
   // Encendido del relé número 2 (Luz Roja)
-  digitalWrite(relay2, HIGH); // Envia señal alta (5V) al segundo relé.
+  digitalWrite(relay2, LOW); // Envia señal BAJA (0V) al segundo relé.
   Serial.println("Relé numero 2 encendido (Luz Roja)");
   delay(45000);           // 45 segundos encendido
   // Apagado del relé número 2
-  digitalWrite(relay2, LOW);  // Envia señal baja (0V) al relé número dos.
+  digitalWrite(relay2, HIGH);  // Envia señal ALTA (5V) al relé número dos.
   Serial.println("Relé 2 apagado (Luz Roja)");
   delay(3000);           // 3 segundos apagado
-
 }
